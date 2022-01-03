5f4820fa19e6307b543a725d49bed0cc72dcea65
ridaarif98 <ridaarif16@gmail.com>
2022-01-03 14:39:24 +0100

    Display all categories

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,11 @@
 class GroupsController < ApplicationController
+    def index
+        @groups = Group.where(user_id: current_user.id)
+    end
+
     def new
         @group = Group.new
+        @icons = icons
     end
 
     def create
@@ -17,5 +22,15 @@ class GroupsController < ApplicationController
 
     def group_params
         params.require(:group).permit(:name, :icon)
+    end
+
+    def icons
+        ['https://i.imgur.com/Ar3Lf3Dt.png',
+        'https://i.imgur.com/R7Q2ZVht.png',
+        'https://i.imgur.com/X99X2Kgt.png',
+        'https://i.imgur.com/bvFovBRt.png',
+        'https://i.imgur.com/mZ6ynFvt.png',
+        'https://i.imgur.com/ZVKPCfjt.png'
+    ]
     end
 end
